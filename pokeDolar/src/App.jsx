b9f3015e4e43bfcf8cf70aa17ded54e0887814d1
LCostaF <146568540+LCostaF@users.noreply.github.com>
2024-11-16 15:55:02 -0300

    feat: Adiciona apresentação de cotação

--- pokeDolar/src/App.jsx
+++ pokeDolar/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PokemonGallery from "./components/PokemonGallery";
+import PokemonDisplay from "./components/PokemonDisplay";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="container">
+      <PokemonDisplay />
       <PokemonGallery />
     </div>
   );
