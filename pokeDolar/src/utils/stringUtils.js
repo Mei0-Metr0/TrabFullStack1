9f65f8b5de020881f46d0af25e02559d62c6b434
LCostaF <146568540+LCostaF@users.noreply.github.com>
2024-11-17 17:05:28 -0300

    refactor: Remove ifs desnecessários

--- pokeDolar/src/utils/stringUtils.js
+++ pokeDolar/src/utils/stringUtils.js
@@ -1,11 +1,9 @@
 // Arquivo com funções utilitárias
 
 export const capitalize = (text) => {
-    if (!text) return ""; // Retorna string vazia caso o input seja inválido
     return text.charAt(0).toUpperCase() + text.slice(1);
 };
 
 export const toUpperCaseText = (text) => {
-    if (!text) return ""; // Retorna string vazia caso o input seja inválido
     return text.toUpperCase();
 };
